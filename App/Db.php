fec5fec0c1e9fb6fff63ad3e2ffd040cc67c37e6
Dizzy <ches@ukr.net>
2016-03-10 15:12:03 +0200

    + метод query() класса Db

--- App/Db.php
+++ App/Db.php
@@ -8,14 +8,30 @@ class Db
 
     function __construct()
     {
-        $this->dbh = new \PDO('mysql:dbname=app;host=127.0.0.1', 'root', '');
+        $this->connect();
     }
-
+        
     public function execute($sql)
     {
         $sth = $this->dbh->prepare($sql);
         $res = $sth->execute();
 
         return $res;
+    }
+
+    public function query($sql)
+    {
+        $sth = $this->dbh->prepare($sql);
+        $res = $sth->execute();
+        if (false !== $res) {
+            return $sth->fetchAll();
+        }
+        
+        return [];
+    }
+
+    private function connect()
+    {
+        $this->dbh = new \PDO('mysql:dbname=app;host=127.0.0.1', 'root', '');
     }
 }
