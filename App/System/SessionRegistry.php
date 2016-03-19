70811f0deafeeab8f7035d203d7e31da27824920
Dizzy <ches@ukr.net>
2016-03-19 22:59:22 +0200

    + Системные классы-заглушки

---
+++ App/System/SessionRegistry.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\System;
+
+
+class SessionRegistry extends Registry
+{
+
+    protected function get($key)
+    {
+        // TODO: Implement get() method.
+    }
+
+    protected function set($key, $value)
+    {
+        // TODO: Implement set() method.
+    }
+}
